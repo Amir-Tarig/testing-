e2db9a4e6826a5e90a09202f3a7ce6912bcea245
Amir <amir_tarig@yahoo.com>
2020-09-25 16:37:09 +0200

    testing

--- test/src/NavBar.vue
+++ test/src/NavBar.vue
@@ -1,11 +1,12 @@
 <template>
  <div class="nav"> 
-      <h2><span>Restaurant</span>Logo</h2>
+    <div class="logo"><h2><span>Restaurant</span>Logo</h2></div> 
     <ul class="navList">
         <li class="navLink">Home</li>
+         <li class="navLink">Menu</li>
            <li class="navLink">About</li>
-              <li class="navLink">contact</li>
-                <li class="navLink">Menu</li>
+              <li class="navLink">Contact</li>
+               
     </ul>
 </div>
 </template>
@@ -23,10 +24,37 @@ export default {
 </script>
 
 <style  scoped>
+
 .nav{
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
+    height: 10vh;
+  
+}
+
+h2{
+    font-size: 13px;
+    color: red;
+}
+
+span{
+    font-size: 2em;
+    color: black;
+}
+
+.logo{
+    text-align: center;
+    width: 50%;
+
+}
+.navList{
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    list-style: none;
 }
 
 </style>
