2f59460cd7bde9661250977a299b20cb5fd6da43
Amir <amir_tarig@yahoo.com>
2020-09-25 15:59:00 +0200

    test

---
+++ test/src/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="nav"> 
+      <h2><span>Restaurant</span>Logo</h2>
+    <ul class="navList">
+        <li class="navLink">Home</li>
+           <li class="navLink">About</li>
+              <li class="navLink">contact</li>
+                <li class="navLink">Menu</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+          
+        }
+    },
+  
+
+}
+</script>
+
+<style  scoped>
+.nav{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+</style>
